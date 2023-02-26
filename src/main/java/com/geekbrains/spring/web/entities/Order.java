6671b2923961f23ff9c7c81a28359bf2f3f65eb9
unknown <zlovred12@gmail.com>
2023-02-26 21:39:37 +0300

    temp

--- src/main/java/com/geekbrains/spring/web/entities/Order.java
+++ src/main/java/com/geekbrains/spring/web/entities/Order.java
@@ -1,4 +1,32 @@
 package com.geekbrains.spring.web.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "total_price")
+    private Integer totalPrice;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
